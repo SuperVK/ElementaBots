81c3cd01f144d8a285e6d5fb540b308daff8399f
victor <victor.klomp@gmail.com>
2019-05-18 14:51:37 +0200

    fixed summoning

--- src/commands/game/summon.js
+++ src/commands/game/summon.js
@@ -11,17 +11,19 @@ module.exports = {
         if(message.args[0] == 'heroic') {
             if(user.items.findIndex(s => s.toLowerCase().startsWith('heroic')) == -1) return message.channel.createMessage(`You don't have`)
             let rng = Math.random()
-            if(rng > 1/3) type = 'legendary'
-            else if(rng > 2/3) type = 'Elite'
+            if(rng < 0.2) type = 'Legendary'
+            else if(rng < 0.5) type = 'Elite'
             else type = 'Rare'
         } else if(message.args[0] == 'basic') {
             if(user.items.findIndex(s => s.toLowerCase().startsWith('basic')) == -1) return message.channel.createMessage(`You don't have`)
-            if(Math.random() > 0.5) type = 'fodder'
-            else type = 'common'
+            if(Math.random() > 0.5) type = 'Fodder'
+            else type = 'Common'
         }
-        let hero = tiers[type][Math.floor(Math.random()*tiers[type].length)]
+        let hero = tiers[type.toLowerCase()][Math.floor(Math.random()*tiers[type.toLowerCase()].length)]
         message.channel.createMessage(`Added ${hero} from type ${type} to your account`)
         user.heroes.push(hero)
+        let index = user.items.findIndex(s => s.toLowerCase().startsWith(message.args[0]) == -1)
+        user.items.splice(index, 1)
         client.saveUser(user)
 
            
