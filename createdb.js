05b4ca9bde03b50252bbd079e4d6e1c1036b56a8
victor <victor.klomp@gmail.com>
2019-05-18 15:26:12 +0200

    remove db.sqlite, add it to gitingore

---
+++ createdb.js
@@ -0,0 +1,15 @@
+const db = require('better-sqlite3')('db.sqlite')
+
+db.prepare(`CREATE TABLE "users" (
+	"id"	TEXT NOT NULL UNIQUE,
+	"items"	TEXT,
+	"heroes"	TEXT,
+	PRIMARY KEY("id")
+)`).run()
+
+db.prepare(`CREATE TABLE "guilds" (
+	"leaderid"	TEXT,
+	"members"	TEXT,
+	"guildid"	TEXT,
+	PRIMARY KEY("guildid")
+)`).run()
