fa8ac44513356a863b34c77c46c78ad24324c037
WovenCoast <wovencoast14782@techie.com>
2019-05-19 14:38:49 +0500

    hopefully this doesn't break xD

--- src/commands/game/profile.js
+++ src/commands/game/profile.js
@@ -1,16 +1,16 @@
 module.exports = {
     aliases: ['profile', 'inv', 'inventory'],
     desc: 'Muchos Profilos',
-    run: async function(message, client) {
+    run: async function (message, client) {
         let user = client.getUser(message.author.id)
         let target = message.content.match(/[0-9]{17,18}/)
-        if(target == null) target = message.author.id 
+        if (target == null) target = message.author.id
         user = client.getUser(target)
         let member = message.channel.guild.members.find(m => m.id == target)
-        switch(message.args[1]) {
+        switch (message.args[1]) {
             case 'additem': {
-                if(!message.member.roles.includes(client.roles.admin)) return message.channel.createMessage(`You don't have enough perms mah boi`)
-                if(message.args[2] == undefined) return message.channel.createMessage(`Plz add something add`)
+                if (!message.member.roles.includes(client.roles.admin)) return message.channel.createMessage(`You don't have enough perms mah boi`)
+                if (message.args[2] == undefined) return message.channel.createMessage(`Plz add something add`)
                 let itemname = message.args.slice(2, message.args.length).join(' ')
                 user.items.push(itemname)
                 client.saveUser(user)
@@ -18,8 +18,8 @@ module.exports = {
                 break;
             }
             case 'addhero': {
-                if(!message.member.roles.includes(client.roles.admin)) return message.channel.createMessage(`You don't have enough perms mah boi`)
-                if(message.args[2] == undefined) return message.channel.createMessage(`Plz add something add`)
+                if (!message.member.roles.includes(client.roles.admin)) return message.channel.createMessage(`You don't have enough perms mah boi`)
+                if (message.args[2] == undefined) return message.channel.createMessage(`Plz add something add`)
                 let heronname = message.args.slice(2, message.args.length).join(' ')
                 user.heroes.push(heronname)
                 client.saveUser(user)
@@ -27,17 +27,17 @@ module.exports = {
                 break;
             }
             default: {
-                
+
                 let heroValue = ''
-                for(let hero of user.heroes) {
+                for (let hero of user.heroes) {
                     heroValue += hero + '\n'
                 }
-                if(user.heroes.length == 0) heroValue += 'None :(\n'
+                if (user.heroes.length == 0) heroValue += 'None :(\n'
                 let itemValue = ''
-                for(let item of user.items) {
+                for (let item of user.items) {
                     itemValue += item + '\n'
                 }
-                if(user.items.length == 0) itemValue += 'None :(\n'
+                if (user.items.length == 0) itemValue += 'None :(\n'
                 message.channel.createMessage({
                     content: `__**${message.channel.guild.members.find(m => m.id == target).username}'s profile**__`,
                     embed: {
