0b2840e3100665669dba368a8a57430487e7d632
Ducky Studios <37939721+dduckfface123@users.noreply.github.com>
2019-05-19 16:48:10 +0100

    Update summon.js

--- src/commands/game/summon.js
+++ src/commands/game/summon.js
@@ -9,13 +9,13 @@ module.exports = {
         if(!(message.args[0] == 'heroic' || message.args[0] == 'basic')) return message.channel.createMessage(`:x: Usage: `)
         let type = ''
         if(message.args[0] == 'heroic') {
-            if(user.items.findIndex(s => s.toLowerCase().startsWith('heroic')) == -1) return message.channel.createMessage(`You don't have`)
+            if(user.items.findIndex(s => s.toLowerCase().startsWith('heroic')) == -1) return message.channel.createMessage(`You don't have the scroll`)
             let rng = Math.random()
             if(rng < 0.2) type = 'Legendary'
             else if(rng < 0.5) type = 'Elite'
             else type = 'Rare'
         } else if(message.args[0] == 'basic') {
-            if(user.items.findIndex(s => s.toLowerCase().startsWith('basic')) == -1) return message.channel.createMessage(`You don't have`)
+            if(user.items.findIndex(s => s.toLowerCase().startsWith('basic')) == -1) return message.channel.createMessage(`You don't have the scroll`)
             if(Math.random() > 0.5) type = 'Fodder'
             else type = 'Common'
         }
